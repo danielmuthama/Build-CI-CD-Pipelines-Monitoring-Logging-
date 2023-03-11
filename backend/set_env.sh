6a56aed587e08d412a4cbd72b5ff59d402c327fa
kabba <danielmuthama23@gmail.com>
2023-03-11 18:37:04 +0300

    update files

---
+++ backend/set_env.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=local
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=localhost
+export TYPEORM_PORT=5532
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=password
+export TYPEORM_DATABASE=glee
