e4fed2aa24563b73b487dbd5d5913b8d60b78787
kabba <danielmuthama23@gmail.com>
2023-03-11 23:24:10 +0300

    update files

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,11 +1,13 @@
-export NODE_ENV=local
-export VERSION=1
+NODE_ENV=local
+VERSION=1
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
-export TYPEORM_HOST=localhost
-export TYPEORM_PORT=5532
+
+# Use these values for the local PG database from the Docker Compose file
+export TYPEORM_HOST=uda-db.c5t6ywv2t7kg.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=password
-export TYPEORM_DATABASE=glee
+export TYPEORM_PASSWORD= iHq8iQa6mYQuWR4V69Oe
+export TYPEORM_DATABASE= LocalPostgres
